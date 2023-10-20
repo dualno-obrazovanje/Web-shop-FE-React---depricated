e83ae70b8ae9aae2e44f6e7644eb28ecfd6494c8
ilijapav <ilija.pavlovic05@gmail.com>
2023-10-20 11:16:14 +0200

    Added login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
+import { TextField, Button } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
+    <div className="form">
+      <h2>Login</h2>
+      <TextField variant="standard" label="Username" />
+      <TextField variant="standard" type="password" label="Password" />
+      <a href="#" className="forgotPassword">Forgot password?</a>
+      <Button variant="contained">Login</Button>
+      <a href="#" className="signUp">Sign up</a>
     </div>
   );
 }
